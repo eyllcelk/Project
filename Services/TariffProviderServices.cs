6f2f77f624f77e1fca40b282f0beea2d885d031a
eyllcelk <79092320+eyllcelk@users.noreply.github.com>
2023-01-10 13:53:08 +0100

    Second Commit

---
+++ Services/TariffProviderServices.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using TariffSelectionApi.Models;
+using System.Linq;
+using TariffSelectionApi.Interfaces;
+
+
+namespace TariffSelectionApi.Services
+{
+    public class TariffProviderServices : ITariffProvider
+    {
+        /// <summary>
+        /// Get/Read list of Tariff providers from json file
+        /// </summary>
+        /// <returns></returns>
+        public List<BaseProduct> GetListOfTariffProviders()
+        {
+
+            using (StreamReader r = new StreamReader("TariffProvider.json"))
+            {
+                string json = r.ReadToEnd();
+
+                return JsonConvert.DeserializeObject<List<BaseProduct>>(json);
+            }
+
+        }
+        /// <summary>
+        /// Get ordered list of tariff providers with annual cost according to consumption 
+        /// </summary>
+        /// <param name="consumption"></param>
+        /// <returns></returns>
+        public List<BaseProduct> GetListOfTariffProvidersWithCons(int consumption)
+        {
+            ProductFactory factory = new ConcreteProductFactory();
+            var tarifs = GetListOfTariffProviders();
+            tarifs.ForEach(item => item.AnnualCost= factory.GetProduct(item).CalculateAnnualCost(consumption));
+            return tarifs.OrderBy(x => x.AnnualCost).ToList();
+        }
+    }
+}
